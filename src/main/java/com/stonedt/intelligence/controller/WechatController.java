8f10a66d315f4f0a103deab55f55ec57cda24f55
ranjunhao <rjh@stonedt.com>
2022-02-23 20:32:43 +0800

    优化更新联系方式

--- src/main/java/com/stonedt/intelligence/controller/WechatController.java
+++ src/main/java/com/stonedt/intelligence/controller/WechatController.java
@@ -8,6 +8,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,8 +35,15 @@ public class WechatController {
     public void weChat(HttpServletRequest request, HttpServletResponse response) throws IOException {
         wechatService.dealevent(request,response);
     }
-	
-	
+
+    @RequestMapping("/wxGroup")
+    public ModelAndView wxGroup(HttpServletRequest request, HttpServletResponse response,ModelAndView mv) throws IOException {
+        String groupid = request.getParameter("groupid");
+        mv.addObject("groupid", groupid);
+        mv.addObject("settingLeft", "wxGroup");
+        mv.setViewName("user/wxGroup");
+        return mv;
+    }
 	
 
 }
