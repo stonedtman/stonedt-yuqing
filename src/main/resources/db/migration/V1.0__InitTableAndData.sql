65705042eae4deaa44f7b20e2fab01f8277f474a
sheng wenxuan <1500862375@qq.com>
2024-01-03 18:17:31 +0800

    数据库更新

--- src/main/resources/db/migration/V1.0__InitTableAndData.sql
+++ src/main/resources/db/migration/V1.0__InitTableAndData.sql
@@ -1015,6 +1015,43 @@ INSERT INTO `default_project` VALUES (4, '2022-06-01 14:00:15', 1531878271678025
 
 SET FOREIGN_KEY_CHECKS = 1;
 
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : conect
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : localhost:3306
+ Source Schema         : stonedt_portal
+
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
+
+ Date: 03/01/2024 18:06:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for wechat_config
+-- ----------------------------
+DROP TABLE IF EXISTS `wechat_config`;
+CREATE TABLE `wechat_config`  (
+                                  `id` int NOT NULL AUTO_INCREMENT,
+                                  `appid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'appid',
+                                  `secret` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'secret',
+                                  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'token',
+                                  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '回调地址',
+                                  `template_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '模板id',
+                                  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+                                  `update_time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 
 
 
