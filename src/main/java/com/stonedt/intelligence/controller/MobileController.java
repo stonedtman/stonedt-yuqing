943f0273140ac794bd650adc4fa5275b995f4c31
sheng wenxuan <1500862375@qq.com>
2024-01-09 19:19:57 +0800

    改为普通controller

--- src/main/java/com/stonedt/intelligence/controller/MobileController.java
+++ src/main/java/com/stonedt/intelligence/controller/MobileController.java
@@ -1,10 +1,10 @@
 package com.stonedt.intelligence.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/mobile")
 public class MobileController {
 
